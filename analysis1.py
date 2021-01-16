4e11d5a0b14b93a358ed92f4ba13e9f0605ae304
hfyjc8 <64581504+hfyjc8@users.noreply.github.com>
2021-01-16 16:03:21 +0800

    Add files via upload

--- analysis1.py
+++ analysis1.py
@@ -23,67 +23,61 @@ def new_window(root):
 
 
 def flood_curve(root,data):
-    if data!=None:
-        new_window(root)
-        plt.clf() # clear plot first
-        pf.floodcurve(data)
-    else:
-        messagebox.showerror("Information", "The data is empty")
+    
+    new_window(root)
+    plt.clf() # clear plot first
+    pf.floodcurve(data)
+    
 def flow_curve(root,data):
-    if data!=None:
-        df=data.copy()
-        new_window(root)
-        plt.clf() # clear plot first
-        pf.flowplot(df)
-    else:
-        messagebox.showerror("Information", "The data is empty")
+    
+    df=data.copy()
+    new_window(root)
+    plt.clf() # clear plot first
+    pf.flowplot(df)
+    
 
 def hydroOnly(root,data):
-    if data!=None:
-        df=data.copy()
-        new_window(root)
-        plt.clf() # clear plot first
-        pf.hydrographOnly(df)
-    else:
-        messagebox.showerror("Information", "The data is empty")
+    
+    df=data.copy()
+    new_window(root)
+    plt.clf() # clear plot first
+    pf.hydrographOnly(df)
+    
 
 def hydro_graph(root,data,data2):
-    if data!=None and data2!=None:
-        df=data.copy()
-        df2=data2.copy()
-        new_window(root)
-        plt.clf() # clear plot first
-        g = gridspec.GridSpec(2, 1, height_ratios=[1, 2])
-        pf.hydrograph(df,df2,g)
-    else:
-        messagebox.showerror("Information", "The data is empty")
+    
+    df=data.copy()
+    df2=data2.copy()
+    new_window(root)
+    plt.clf() # clear plot first
+    g = gridspec.GridSpec(2, 1, height_ratios=[1, 2])
+    pf.hydrograph(df,df2,g)
+    
 
 
 def medianDischarge(root,data):
-    if data!=None:
-        df=data.copy()
-        new_window(root)
-        plt.clf() # clear plot first
-        pf.medianmonthDis(df)
-    else:
-        messagebox.showerror("Information", "The data is empty")
+    
+    df=data.copy()
+    new_window(root)
+    plt.clf() # clear plot first
+    pf.medianmonthDis(df)
+    
 
 
 
 def median_Rain(root,data2):
-    if data2!=None:
-        df=data2.copy()
-        l=len(data2.columns)-1
-        Top2=tk.Toplevel(root)
-        var = tk.IntVar()
-        var.set(0)
+    
+    df=data2.copy()
+    l=len(data2.columns)-1
+    Top2=tk.Toplevel(root)
+    var = tk.IntVar()
+    var.set(0)
 
-        tk.Radiobutton(Top2,text='all columns',variable=var, value=0).pack(anchor=tk.W)
-        for i in range(l):
-            b=i+1
-            tk.Radiobutton(Top2,text=data2.columns[i+1],variable=var, value=b).pack(anchor=tk.W)
-    else:
-        messagebox.showerror("Information", "The data is empty")
+    tk.Radiobutton(Top2,text='all columns',variable=var, value=0).pack(anchor=tk.W)
+    for i in range(l):
+        b=i+1
+        tk.Radiobutton(Top2,text=data2.columns[i+1],variable=var, value=b).pack(anchor=tk.W)
+    
     
     def _continue():
         g=var.get()
