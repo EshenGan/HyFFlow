d818d517550ef9998f8dc92c19e6038353e98b97
hinda123 <72979365+hinda123@users.noreply.github.com>
2021-01-29 14:58:47 +0800

    Update analysis1.py

--- analysis1.py
+++ analysis1.py
@@ -4,31 +4,66 @@ from matplotlib import gridspec
 import plotfunction as pf
 import matplotlib
 matplotlib.use('TkAgg')
+from tkinter.filedialog import asksaveasfile
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 from pandas import DataFrame
 from tkinter import ttk, filedialog, messagebox
 
-def new_window(root):
-    fig ,ax= plt.subplots(figsize=(12,6))
-    Top=tk.Toplevel(root)
-    canvas = FigureCanvasTkAgg(fig, master=Top)
-    plot_widget = canvas.get_tk_widget()
-    def _quit():
-        plt.clf() 
-        Top.destroy()
-    tk.Button(Top, text = 'Quit', command = _quit).grid(row=2, column=0)   
-    plot_widget.grid(row=0, column=0)
+from matplotlib.backends.backend_pdf import PdfPages
+
+
 
 
 def flood_curve(root,data):
+    def new_window(root):
+        fig, ax = plt.subplots(figsize=(12, 6))
+        Top = tk.Toplevel(root)
+        canvas = FigureCanvasTkAgg(fig, master=Top)
+        plot_widget = canvas.get_tk_widget()
+
+        def _quit():
+            plt.clf()
+            Top.destroy()
+
+        tk.Button(Top, text='Quit', command=_quit).grid(row=2, column=1)
+        plot_widget.grid(row=0, column=0)
+
+        def Save():
+            plt.savefig('Flood frequency.png')
+
+        tk.Button(Top, text='Save', command=Save).grid(row=2, column=0)
+        plot_widget.grid(row=0, column=0)
     
     new_window(root)
     plt.clf() # clear plot first
     pf.floodcurve(data)
+
+
+
+
     
 def flow_curve(root,data):
+
+    def new_window(root):
+        fig, ax = plt.subplots(figsize=(12, 6))
+        Top = tk.Toplevel(root)
+        canvas = FigureCanvasTkAgg(fig, master=Top)
+        plot_widget = canvas.get_tk_widget()
+
+        def _quit():
+            plt.clf()
+            Top.destroy()
+
+        tk.Button(Top, text='Quit', command=_quit).grid(row=2, column=1)
+        plot_widget.grid(row=0, column=0)
+
+        def Save():
+            plt.savefig('Flow duration.png')
+
+        tk.Button(Top, text='Save', command=Save).grid(row=2, column=0)
+        plot_widget.grid(row=0, column=0)
     
     df=data.copy()
     new_window(root)
@@ -37,6 +72,25 @@ def flow_curve(root,data):
     
 
 def hydroOnly(root,data):
+
+    def new_window(root):
+        fig, ax = plt.subplots(figsize=(12, 6))
+        Top = tk.Toplevel(root)
+        canvas = FigureCanvasTkAgg(fig, master=Top)
+        plot_widget = canvas.get_tk_widget()
+
+        def _quit():
+            plt.clf()
+            Top.destroy()
+
+        tk.Button(Top, text='Quit', command=_quit).grid(row=2, column=1)
+        plot_widget.grid(row=0, column=0)
+
+        def Save():
+            plt.savefig('hydroOnly.png')
+
+        tk.Button(Top, text='Save', command=Save).grid(row=2, column=0)
+        plot_widget.grid(row=0, column=0)
     
     df=data.copy()
     new_window(root)
@@ -45,17 +99,60 @@ def hydroOnly(root,data):
     
 
 def hydro_graph(root,data,data2):
-    
+
+    def new_window(root):
+        fig, ax = plt.subplots(figsize=(12, 6))
+        Top = tk.Toplevel(root)
+        canvas = FigureCanvasTkAgg(fig, master=Top)
+        plot_widget = canvas.get_tk_widget()
+
+
+        def _quit():
+            plt.clf()
+            Top.destroy()
+
+        tk.Button(Top, text='Quit', command=_quit).grid(row=2, column=1)
+        plot_widget.grid(row=0, column=0)
+
+        def Save():
+
+            plt.savefig('Hydrograph and hyetrograph.png')
+
+        tk.Button(Top, text='Save', command=Save).grid(row=2, column=0)
+        plot_widget.grid(row=0, column=0)
+
     df=data.copy()
     df2=data2.copy()
     new_window(root)
     plt.clf() # clear plot first
     g = gridspec.GridSpec(2, 1, height_ratios=[1, 2])
     pf.hydrograph(df,df2,g)
+
+
+
     
 
 
 def medianDischarge(root,data):
+
+    def new_window(root):
+        fig, ax = plt.subplots(figsize=(12, 6))
+        Top = tk.Toplevel(root)
+        canvas = FigureCanvasTkAgg(fig, master=Top)
+        plot_widget = canvas.get_tk_widget()
+
+        def _quit():
+            plt.clf()
+            Top.destroy()
+
+        tk.Button(Top, text='Quit', command=_quit).grid(row=2, column=1)
+        plot_widget.grid(row=0, column=0)
+
+        def Save():
+            plt.savefig('Median Discharge.png')
+
+        tk.Button(Top, text='Save', command=Save).grid(row=2, column=0)
+        plot_widget.grid(row=0, column=0)
     
     df=data.copy()
     new_window(root)
@@ -66,6 +163,25 @@ def medianDischarge(root,data):
 
 
 def median_Rain(root,data2):
+
+    def new_window(root):
+        fig, ax = plt.subplots(figsize=(12, 6))
+        Top = tk.Toplevel(root)
+        canvas = FigureCanvasTkAgg(fig, master=Top)
+        plot_widget = canvas.get_tk_widget()
+
+        def _quit():
+            plt.clf()
+            Top.destroy()
+
+        tk.Button(Top, text='Quit', command=_quit).grid(row=2, column=1)
+        plot_widget.grid(row=0, column=0)
+
+        def Save():
+            plt.savefig('Median RainFall.png')
+
+        tk.Button(Top, text='Save', command=Save).grid(row=2, column=0)
+        plot_widget.grid(row=0, column=0)
     
     df=data2.copy()
     l=len(data2.columns)-1
@@ -91,6 +207,7 @@ def median_Rain(root,data2):
     
 
     
+
 
 
 
