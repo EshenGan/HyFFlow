a419c6b5e72050080e82de3f677482b9a657ea4d
Dukey <61282110+EshenGan@users.noreply.github.com>
2021-04-27 12:28:33 +0800

    Add files via upload

--- save.py
+++ save.py
@@ -7,19 +7,34 @@ matplotlib.use('TkAgg')
 
 
 def savepng1(filename):
-    fullfilename = filename + time.strftime(" %Y-%m-%d %H%M%S") + ".png"
-    path = os.path.abspath('SavedFiles')
-    plt.savefig(os.path.join(path, fullfilename))
+    fullfilename = filename + time.strftime(" %Y-%m-%d %H%M%S") + ".png"  # filename string with timestamp
+    path = os.path.abspath('SavedFiles')  # path to folder named SavedFiles
+    # check if there is such folder exist
+    # if doesnt, create a new one with specific folder name
+    if not os.path.exists(path):
+        os.mkdir(path)
+        plt.savefig(os.path.join(path, fullfilename))
+    # if exist, use it
+    else:
+        plt.savefig(os.path.join(path, fullfilename))  # save plots as images with filename specified above in path specified above
 
 
 def savepng2(filename, figure):
     fullfilename = filename + time.strftime(" %Y-%m-%d %H%M%S") + ".png"
     path = os.path.abspath('SavedFiles')
-    figure.savefig(os.path.join(path, fullfilename))
+    if not os.path.exists(path):
+        os.mkdir(path)
+        figure.savefig(os.path.join(path, fullfilename))
+    else:
+        figure.savefig(os.path.join(path, fullfilename))  # save plots as images with filename specified above in path specified above
 
 
 def saveexcel(filename, model):
     aov_table = sm.stats.anova_lm(model)
     fullfilename = filename + time.strftime(" %Y-%m-%d %H%M%S")+".xlsx"
     path = os.path.abspath('SavedFiles')
-    aov_table.to_excel(os.path.join(path, fullfilename))
+    if not os.path.exists(path):
+        os.mkdir(path)
+        aov_table.to_excel(os.path.join(path, fullfilename))
+    else:
+        aov_table.to_excel(os.path.join(path, fullfilename))  # save plots as images with filename specified above in path specified above
