336f433ba2ecf313047511fcf910268bdf1b7e96
Dukey <61282110+EshenGan@users.noreply.github.com>
2021-04-27 14:25:27 +0800

    Update loadingPage.py

--- loadingPage.py
+++ loadingPage.py
@@ -2,10 +2,11 @@ from tkinter import *
 import main as mn
 import usermanual as um
 
-
+# setting up the UI window
 loadroot = Tk()
 loadroot.title("HyFFlow")
 loadroot.geometry("415x410")
+loadroot.iconbitmap('iconlogo.ico')
 loadroot.resizable(0, 0)
 
 # Title label
@@ -23,20 +24,20 @@ guidebutton = Button(loadroot, text="GUIDE", height="5", width="40", bg="lightbl
                      command=lambda: um.openguide(loadroot))  
 guidebutton.place(x=60, y=170)
 
-
+# function to exit the application
 def exitt():
     loadroot.destroy()
     loadroot.quit()
 
-
+# exit button 
 exitbutton = Button(loadroot, text="EXIT", height="5", width="40", bg="lightblue", fg="black", command=exitt)
 exitbutton.place(x=60, y=270)
 
-
+# function to close UI window if x is click instead of EXIT
 def quit_me():
     loadroot.quit()
     loadroot.destroy()
 
-
+# close UI window if x is click instead of EXIT
 loadroot.protocol("WM_DELETE_WINDOW", quit_me)
 loadroot.mainloop()
